32db441be1ee28a96cb8953bcfc6e6ded92ef852
ryanhardin <rhardin23.rh@gmail.com>
2020-06-14 08:56:46 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+require("dotenv").config();
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`,
+      )
+      .then((movie) => setMovies(movie.data.results))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const display = movies
+    ? movies.map((movie) => (
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "20%" }} key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title} />
+          <h2 style={{ textAlign: "center" }}>{movie.title}</h2>
+        </div>
+      ))
+    : null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Movies Now Playing</h1>
+      <div style={{ display: "flex", justifyContent: "space-evenly", flexWrap: "wrap" }}>{display}</div>
     </div>
   );
 }
