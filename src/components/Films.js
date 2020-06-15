36c46c55e8d57699b531810d961844be0c19f09e
ryanhardin <rhardin23.rh@gmail.com>
2020-06-15 15:14:43 -0400

    added information from api

--- src/components/Films.js
+++ src/components/Films.js
@@ -3,12 +3,17 @@ import "../css/Film.css";
 
 const Films = ({ movies }) => {
   const list = movies.map((movie) => (
-    <div>
+    <div style={styles.filmsContainer} key={movie.id}>
       <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title} />
-      <h2 className="header">{movie.title}</h2>
+      <div>
+        <h1 className="header">{movie.title}</h1>
+        <p>Release Date: {movie.release_date}</p>
+        <p>Popularity: {movie.popularity}</p>
+        <p>{movie.overview}</p>
+      </div>
     </div>
   ));
-
+  console.log(movies);
   return <div style={styles.container}>{list}</div>;
 };
 
@@ -17,6 +22,12 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     margin: "10px",
+  },
+
+  filmsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    margin: "15px 0",
   },
 };
 
