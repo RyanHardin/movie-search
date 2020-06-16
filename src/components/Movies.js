05be9af363c595220bec30a31b7792255fa28ba2
ryanhardin <rhardin23.rh@gmail.com>
2020-06-16 10:37:06 -0400

    change to get request

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -16,7 +16,7 @@ const Movies = () => {
       .get(
         `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`,
       )
-      .then((movie) => setMovies(movie.data.results))
+      .then((res) => setMovies(res.data.results))
       .catch((error) => console.log(error));
   }, []);
 
